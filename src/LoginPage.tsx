3aa9a4369ac1c9bf44cb0bca49800e72164d4469
CerberusProgrammer <cerberusprogrammer@gmail.com>
2024-08-16 08:20:57 -0700

    New components

---
+++ src/LoginPage.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <input type="text" placeholder="email" />
+      <input type="password" placeholder="password" />
+      <button>Login with Google</button>
+      <button>Login</button>
+    </div>
+  );
+}
