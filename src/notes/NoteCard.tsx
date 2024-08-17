bc4118e3ad1664a3dcab6ed8539b061a4264b428
sazarcode <cerberusprogrammer@gmail.com>
2024-08-17 12:58:46 -0700

    feat: Add ability to edit note title in NoteCard component

--- src/notes/NoteCard.tsx
+++ src/notes/NoteCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Note } from "./NotesState";
 import "./NoteCard.css";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
@@ -6,12 +7,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 type Props = {
   note: Note;
   onTap: () => void;
+  onUpdateTitle: (noteId: string, newTitle: string) => void;
 };
 
-export default function NoteCard({ note, onTap }: Props) {
+export default function NoteCard({ note, onTap, onUpdateTitle }: Props) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(note.title);
+
+  const handleTitleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const handleTitleBlur = () => {
+    setIsEditing(false);
+    if (title !== note.title) {
+      onUpdateTitle(note.id, title);
+    }
+  };
+
   return (
     <div className="note-card">
-      <h1>{note.title}</h1>
+      {isEditing ? (
+        <input
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+          onBlur={handleTitleBlur}
+          className="note-title-input"
+        />
+      ) : (
+        <h1 onClick={handleTitleClick}>{note.title}</h1>
+      )}
       <p>{note.content}</p>
       <small>Created At: {note.createdAt.toDate().toString()}</small>
       <small>Updated At: {note.updatedAt.toDate().toString()}</small>
