d2131387bc701d38a35c603eeaf823c8deb4d442
Omar Flores <cerberusprogrammer@gmail.com>
2024-08-16 21:14:15 -0700

    refactor: Update UserContext with login and logout functionality

--- src/providers/UserContext.ts
+++ src/providers/UserContext.ts
@@ -5,7 +5,13 @@ import { UserActions } from "./UserActions";
 export const UserContext = createContext<{
   state: UserState;
   dispatch: Dispatch<UserActions>;
+  loginWithEmail: (email: string, password: string) => Promise<void>;
+  loginWithGoogle: () => Promise<void>;
+  logout: () => Promise<void>;
 }>({
   state: initialUserState,
   dispatch: () => {},
+  loginWithEmail: async () => {},
+  loginWithGoogle: async () => {},
+  logout: async () => {},
 });
