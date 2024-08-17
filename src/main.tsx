bd744e0fa617a5dc1e9c2de7f33f687e03840f86
Omar Flores <cerberusprogrammer@gmail.com>
2024-08-16 21:14:00 -0700

    refactor: Remove UserProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes.tsx";
-import UserProvider from "./providers/UserProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <UserProvider>
-      <RouterProvider router={routes} />
-    </UserProvider>
+    <RouterProvider router={routes} />
   </StrictMode>
 );
