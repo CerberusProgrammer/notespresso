e3bbe52242e8e1b5816fb4af6d0998a299b0ee44
Omar Flores <cerberusprogrammer@gmail.com>
2024-08-16 21:13:46 -0700

    refactor: Add logout functionality to HomePage

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,3 +1,17 @@
+import { useContext } from "react";
+import { UserContext } from "./providers/UserContext";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const { logout } = useContext(UserContext);
+
+  const handleLogout = async () => {
+    await logout();
+  };
+
+  return (
+    <div>
+      <div>HomePage</div>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 }
