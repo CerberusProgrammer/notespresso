e975d604f147470fc65dd5c0712c689911f12715
Omar Flores <cerberusprogrammer@gmail.com>
2024-08-16 20:26:47 -0700

    feat: Update main.tsx to use RouterProvider and UserProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import LoginPage from "./LoginPage.tsx";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes.tsx";
+import UserProvider from "./providers/UserProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LoginPage />
+    <UserProvider>
+      <RouterProvider router={routes} />
+    </UserProvider>
   </StrictMode>
 );
